8cdc82259e835f4db7f9529bd00beb73b7ea1870
Robin Lemaire <robin.lemaire@adevinta.com>
2024-06-21 15:23:28 +0200

    Migrate from spark-ios

---
+++ Tests/SnapshotTests/View/SwiftUI/TextLinkViewSnapshotTests.swift
@@ -0,0 +1,57 @@
+//
+//  TextLinkViewSnapshotTests.swift
+//  SparkTextLinkSnapshotTests
+//
+//  Created by robin.lemaire on 19/12/2023.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import SparkTextLink
+import SparkTheming
+import SparkTheme
+@_spi(SI_SPI) import SparkCommon
+@_spi(SI_SPI) import SparkCommonSnapshotTesting
+import SwiftUI
+
+final class TextLinkViewSnapshotTests: SwiftUIComponentSnapshotTestCase {
+
+    // MARK: - Properties
+
+    private let theme: Theme = SparkTheme.shared
+
+    // MARK: - Tests
+
+    func test() {
+        let scenarios = TextLinkScenarioSnapshotTests.allCases
+
+        for scenario in scenarios {
+            let configurations: [TextLinkConfigurationSnapshotTests] = scenario.configuration(
+                isSwiftUIComponent: true
+            )
+
+            for configuration in configurations {
+                let view = TextLinkView(
+                    theme: self.theme,
+                    text: configuration.type.text,
+                    textHighlightRange: configuration.type.textHighlightRange,
+                    intent: configuration.intent,
+                    typography: configuration.size.typography,
+                    variant: configuration.variant,
+                    image: configuration.image?.rightValue,
+                    alignment: configuration.alignment,
+                    action: {}
+                )
+                    .fixedSize()
+
+                self.assertSnapshot(
+                    matching: view,
+                    modes: configuration.modes,
+                    sizes: configuration.sizes,
+                    testName: configuration.testName()
+                )
+            }
+        }
+    }
+}
